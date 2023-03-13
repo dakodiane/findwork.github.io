5da3b27d02864cf86f7770db8bcfabcc1fe49515
dakodiane <dakodiane470@gmail.com>
2023-03-13 11:47:47 +0100

    initial commit

---
+++ jobbenin/resources/views/components/header.blade.php
@@ -0,0 +1,51 @@
+<header>
+        <!-- Header Start -->
+       <div class="header-area header-transparrent">
+           <div class="headder-top header-sticky">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-3 col-md-2">
+                            <!-- Logo -->
+                            <div class="logoo">
+                                <a href="index.html" ><img src="{{asset('img/logo/logojob.png')}}" alt=""></a>
+                            </div>  
+                        </div>
+                        <div class="col-lg-9 col-md-9">
+                            <div class="menu-wrapper">
+                                <!-- Main-menu -->
+                                <div class="main-menu">
+                                    <nav class="d-none d-lg-block">
+                                        <ul id="navigation">
+                                            <li><a href="#">Accueil</a></li>
+                                            <li><a href="">Trouver une offre </a></li>
+                                            <li><a href="">Espace Freelancer</a></li>
+                                            <li><a href="">A propos</a></li>
+                                           
+                                            <li class="align-middle">                                    
+                                                <a href="" class="btn head-btn2 ">Se Connecter</a>
+                                            </li>
+                                            <li class="align-middle"><a href="#" class=" btn head-btn1">S'inscrire</a>
+                                                <ul class="submenu ">
+                                                    <li><a href="">Postulant</a></li>
+                                                    <li><a href="">Recruteur</a></li>
+                                                    <li><a href="">Freelancer</a></li>
+                                              </ul>
+                                            </li>
+
+
+                                        </ul>
+                                    </nav>
+                                </div>          
+                               
+                            </div>
+                        </div>
+                         <!-- Mobile Menu -->
+                         <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+       </div>
+        <!-- Header End -->
+    </header>
---
+++ jobbenin/resources/views/templates/app.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+         <title>Job_Benin </title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="manifest" href="site.webmanifest">
+		<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+        
+        <link rel="stylesheet" href="{{ asset('') }}">
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
+        <link rel="stylesheet" href="{{asset('css/price_rangs.css')}}">
+        <link rel="stylesheet" href="{{asset('css/slicknav.css')}}">
+        <link rel="stylesheet" href="{{asset('css/animate.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/animate.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/magnific.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/fontawesome-all.min.css')}}">
+        
+        <link rel="stylesheet" href="{{asset('css/themify-icons.css')}}">
+        <link rel="stylesheet" href="{{asset('css/slick.css')}}">
+        <link rel="stylesheet" href="{{asset('css/nice-select.css')}}">
+        <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/css/ionicons.min.css">
+          
+
+		<!-- All JS Custom Plugins Link Here here -->
+       
+        <script src="{{asset('js/vendor/modernizr-3.5.0.min.js')}}"></script>
+		<!-- Jquery, Popper, Bootstrap -->
+        <script src="{{asset('js/vendor/jquery-1.12.4.min.js')}}"></script>
+        <script src="{{asset('js/popper.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap.min.js')}}"></script>
+      
+	    <!-- Jquery Mobile Menu -->
+        <script src="{{asset('js/jquery.slicknav.min.js')}}"></script>
+
+		<!-- Jquery Slick , Owl-Carousel Plugins -->
+        <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+
+        <script src="{{asset('js/slick.min.js')}}"></script>
+
+        <script src="{{asset('js/price_rangs.js')}}"></script>
+
+       
+        
+		<!-- One Page, Animated-HeadLin -->
+        <script src="{{asset('js/wow.min.js')}}"></script>
+        <script src="{{asset('js/animated.headline.js')}}"></script>
+        <script src="{{asset('js/jquery.magnific-popup.js')}}"></script>
+
+    
+
+		<!-- Scrollup, nice-select, sticky -->
+        <script src="{{asset('js/jquery.scrollUp.min.js')}}"></script>
+        <script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
+        <script src="{{asset('js/jquery.sticky.js')}}"></script>
+
+        
+        <!-- contact js -->
+        <script src="{{asset('js/contact.js')}}"></script>
+        <script src="{{asset('js/jquery.form.js')}}"></script>
+        <script src="{{asset('js/jquery.validate.min.js')}}"></script>
+        <script src="{{asset('js/mail-script.js')}}"></script>
+        <script src="{{asset('js/jquery.ajaxchimp.min.js')}}"></script>
+
+        
+		<!-- Jquery Plugins, main Jquery -->	
+        <script src="{{asset('js/plugins.js')}}"></script>
+        <script src="{{asset('js/main.js')}}"></script>
+        <script src="{{asset('js/jquery.min.js')}}"></script>
+        <script src="{{asset('js/popper.js')}}"></script>
+        <script src="{{asset('js/caro.js')}}"></script>
+
+</head>
+<body>
+@include('components.header')
+
+
+@yield('document')
+
+@include('components.footer')
+
+
+</body>
+</html>
