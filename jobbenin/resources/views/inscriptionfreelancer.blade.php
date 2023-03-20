985d615ba0b448d68f734436fb1c93b1e2dddb92
Djemiiii <titilayoakponne@gmail.com~>
2023-03-20 11:30:26 +0100

    modificationfichierInscriptionpostulant

--- jobbenin/resources/views/inscriptionfreelancer.blade.php
+++ jobbenin/resources/views/inscriptionfreelancer.blade.php
@@ -34,6 +34,25 @@
                                                     placeholder="Entrer vos Prénoms">
                                             </div>
                                         </div>
+                                        <div class="row form-group ">
+                                            <div class="col-md-12 mb-3 mb-md-0">
+                                                <label class="text-black" for="fname"> Photo Professionnelle</label>
+                                                    <div class="description">Les fichiers doivent peser moins de
+                                                        <strong>2 Mo</strong>.<br />
+                                                        Extensions autorisées :<strong>png jpeg pdf</strong>.</div>
+                                                      <div class="file-widget form-managed-file clearfix">
+                                                       <input type="file" id="edit-field-inscription-und-0-upload"
+                                                        name="files[field_inscription_und_0]" size="50" class="form-file" />
+                                                        <input type="hidden" name="field_inscription_[und][0][fid]" value="0" />
+                               
+                               
+                                                     <input type="hidden" name="field_inscription_[und][0][display]" value="1" />
+
+                                                     </div>
+
+                                            </div>
+                                            </div>
+
 
                                         <div class="row form-group">
                                             <div class="col-md-12 mb-3 mb-md-0">
