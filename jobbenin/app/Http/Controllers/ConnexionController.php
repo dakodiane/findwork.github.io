38441b08ce8f76949a620f280d2d2daaebfc4ec9
dakodiane <dakodiane470@gmail.com>
2023-03-30 12:36:24 +0100

    deconnexion

--- jobbenin/app/Http/Controllers/ConnexionController.php
+++ jobbenin/app/Http/Controllers/ConnexionController.php
@@ -62,7 +62,13 @@ class ConnexionController extends Controller
         }
     }
 
-
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
     /**
      * Show the form for creating a new resource.
      */
