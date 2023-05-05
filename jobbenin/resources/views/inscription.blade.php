61ae5863f9a23a1a53235389eb8d3395b4b9b86f
dakodiane <dakodiane470@gmail.com>
2023-05-05 15:51:02 +0100

    10

--- jobbenin/resources/views/inscription.blade.php
+++ jobbenin/resources/views/inscription.blade.php
@@ -6,7 +6,7 @@
     <section class="site-section">
         <div class="container">
             <div class="row justify-content-center align-items-center">
-                <div class="card o-hidden border-0 shadow-lg my-5" style="width:44em;">
+            <div class="card o-hidden border-0 shadow-lg my-5 col-lg-8 col-md-10 col-sm-12 mx-auto">
                     <div class="" style="justify-content: center;">
                         <div class="text-center">
                             <h1 class="h4 text-gray-900 mb-4">
@@ -16,23 +16,24 @@
                                 <b> <u>Inscrivez-vous à JobBénin!</u> </b>
                             </h1>
                         </div>
-                        <div class="col-lg-6">
+                        <div class="col-lg-6 col-md-10 col-sm-12 mx-auto">
+         
 
                             <form action="{{'inscription'}}" method="POST" class="p-4 border rounded" id="forminsc">
                                 @csrf
                                 <div>
 
-                                <select class="form-select form-select-lg mb-3 @error('role') is-invalid @enderror" name="role" aria-label=".form-select-lg example">
-                                    <option selected>Choisissez votre type de compte</option>
-                                    <option value="recruteur">Recruteur</option>
-                                    <option value="postulant">Postulant</option>
-                                    <option value="freelancer">Freelancer</option>
-                                </select>
+                                    <select class="form-select form-select-lg mb-3 @error('role') is-invalid @enderror" name="role" aria-label=".form-select-lg example">
+                                        <option selected>Choisissez votre type de compte</option>
+                                        <option value="recruteur">Recruteur</option>
+                                        <option value="postulant">Postulant</option>
+                                        <option value="freelancer">Freelancer</option>
+                                    </select>
 
-                                @error('role')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
+                                    @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                                     @enderror
 
 
@@ -45,7 +46,7 @@
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
-                                    @enderror
+                                        @enderror
                                     </div>
                                 </div>
 
@@ -57,7 +58,7 @@
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
-                                    @enderror
+                                        @enderror
                                     </div>
                                 </div>
                                 <div class="row form-group">
@@ -68,14 +69,14 @@
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
-                                    @enderror
+                                        @enderror
                                     </div>
                                 </div>
                                 <div class="row form-group mb-4">
                                     <div class="col-md-12 mb-3 mb-md-0">
                                         <label class="text-black" for="fname">Confirmer le Mot de Passe</label>
                                         <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" placeholder="Confirmez votre mot de Passe ">
-                                        
+
                                     </div>
                                 </div>
 
@@ -83,30 +84,30 @@
                                 <div class="row form-group">
                                     <div class="col-md-12">
                                         <a href="">
-                                        <button type="submit"  class="btn px-4 btn-primary text-white" data-toggle="modal" data-target="#inscriptionModal">S'inscrire</button>
+                                            <button type="submit" class="btn px-4 btn-primary text-white" data-toggle="modal" data-target="#inscriptionModal">S'inscrire</button>
 
-                                             </a>
-                                             <script src="{{ asset('js/jquery.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<!-- Modal -->
-<div class="modal fade" id="inscriptionModal" tabindex="-1" role="dialog" aria-labelledby="inscriptionModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="inscriptionModalLabel">Inscription</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Veuillez vérifier votre boîte mail pour confirmer votre inscription.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-      </div>
-    </div>
-  </div>
-</div>
+                                        </a>
+                                        <script src="{{ asset('js/jquery.min.js') }}"></script>
+                                        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="inscriptionModal" tabindex="-1" role="dialog" aria-labelledby="inscriptionModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="inscriptionModalLabel">Inscription</h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <p>Veuillez vérifier votre boîte mail pour confirmer votre inscription.</p>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
 
 
 
@@ -119,7 +120,7 @@
                                 </div>
 
                             </form>
-                            
+
                         </div>
 
                     </div>
--- jobbenin/resources/views/postuleroffre.blade.php
+++ jobbenin/resources/views/postuleroffre.blade.php
@@ -3,133 +3,131 @@
 @section('document')
 
 <main>
-    
-        <section class="site-section">
-            <div class="container" style="width:45em;">
 
-                <div class="row justify-content-center align-items-center">
-                    <div class="card o-hidden border-0 shadow-lg my-5" >
-                        <div class="card-body p-1" style="">
-                            <div class="" style="justify-content: center;">
-                                <p></p><p></p>
-                                <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">
-                                        <b> <u>Déposer Votre Candidature</u> </b>
-                                    </h1>
-                                </div>
+  <section class="site-section">
+    <div class="container" style="width:45em;">
 
-                                <div class="col-sm-11 mx-auto">
-                    <div class="container">
- <div class="col-md-12" 
-        style="background-color:#1f3bb3;float:left; width: 477px;">
-            
-            <div class="alert -primary alert-dismissible fade show"
-              style="background-color:#1f3bb3"role="alert">
-                        <strong class="text" style="background-color:#1f3bb3;color:white"> 
-                     Hello !   {{ $user->name }}</strong>
+      <div class="row justify-content-center align-items-center">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-1" style="">
+            <div class="" style="justify-content: center;">
+              <p></p>
+              <p></p>
+              <div class="text-center">
+                <h1 class="h4 text-gray-900 mb-4">
+                  <b> <u>Déposer Votre Candidature</u> </b>
+                </h1>
+              </div>
 
-                <p class="text-light my-0">
-                    Besoin d'un CV Professionnel pour votre Candidature ?
-                </p>
-                <p class="mt-3 mb-0"> <a href="https://wa.me/0022990733200"target="_blank" >
-            <button  style="background-color:white;border:none; color:#1f3bb3;border-radius:40px"  >
-            Commander Maintenant</button>    
-  </a> </p>
-        
-                </div>
-             
-        </div>
-  <p></p>
+              <div class="col-sm-11 mx-auto">
+                <div class="container">
+                  <div class="col-md-12" style="background-color:#1f3bb3;float:left; width: 477px;">
 
-  <form method="POST" action="{{route('postuleroffre', ['id_offre' => $offre->id])}}" enctype="multipart/form-data" style="max-width: 400px; margin: 0 auto;">
-       
-       @csrf
-       <div class="form-group">
-         <div class="text-format-wrapper">
-           <div class="form-item form-lettre_motivation wysiwyg-wrapper form-type-textarea form-item-lettre_motivation-value" id="edit-lettre_motivation-value-wrapper">
-             <label for="edit-lettre_motivation" style="padding-top:2em">Lettre  de Motivation </label>
-             <div class="form-textarea-wrapper resizable">
-               <textarea class="" name="lettre_motivation" id="lettre_motivation" cols="16" rows="10" required="required" placeholder=""></textarea>
-             </div>
-           </div>
-         </div>
-       </div>
-       <div class="form-group">
-         <label for="secteur">Curriculum Vitae (CV) </label>
-         <input type="file" id="cv" name="cv" required="required" class="form-control" placeholder="" style="width:34em">
-       </div>
-       <div class="row form-group">
-         <div class="col-md-8 col-sm-12">
-           <button type="submit" class="btn btn-block btn-primary btn-md" id="submit-btn"> Postuler</button>
-         </div>
-         <div class="col-md-4 col-sm-0 text-end">
-           <!-- Utilisez cette colonne pour espacer le bouton sur les grands écrans -->
-         </div>
-       </div>
-     </form>
-     
+                    <div class="alert -primary alert-dismissible fade show" style="background-color:#1f3bb3" role="alert">
+                      <strong class="text" style="background-color:#1f3bb3;color:white">
+                        Hello ! {{ $user->name }}</strong>
 
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" 
-aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel">JobBénin</h4>
-            </div>
-            <div class="modal-body">
-                 <p> <b> Succès!</b></p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
-            </div>
-        </div>
-    </div>
-</div>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-  $(document).ready(function() {
-    $('#my-form').on('submit', function(e) {
-      var isFormValid = true;
-      var form = $(this);
+                      <p class="text-light my-0">
+                        Besoin d'un CV Professionnel pour votre Candidature ?
+                      </p>
+                      <p class="mt-3 mb-0"> <a href="https://wa.me/0022990733200" target="_blank">
+                          <button style="background-color:white;border:none; color:#1f3bb3;border-radius:40px">
+                            Commander Maintenant</button>
+                        </a> </p>
 
-      // Check if all required fields are filled
-      form.find('[required]').each(function() {
-        if (!$(this).val()) {
-          isFormValid = false;
-          $(this).addClass('is-invalid');
-        }
-      });
-
-    
-      if (!isFormValid) {
-        e.preventDefault();
-        return false;
-      }
-
-
-      form.unbind('submit').submit();
-
-      $('#myModal').modal('show');
-
-    
-      e.preventDefault();
-      return false;
-    });
-  });
-</script>
-
-    </div>
-</div>
-   
-                            </div>
-                        </div>
                     </div>
-                </div>
-            </div>
-            </div>
-        </section>
 
-    
+                  </div>
+                  <p></p>
+
+                  <form method="POST" action="{{route('postuleroffre', ['id_offre' => $offre->id])}}" enctype="multipart/form-data" style="max-width: 400px; margin: 0 auto;">
+
+                    @csrf
+                    <div class="form-group">
+                      <div class="text-format-wrapper">
+                        <div class="form-item form-lettre_motivation wysiwyg-wrapper form-type-textarea form-item-lettre_motivation-value" id="edit-lettre_motivation-value-wrapper">
+                          <label for="edit-lettre_motivation" style="padding-top:2em">Lettre de Motivation </label>
+                          <div class="form-textarea-wrapper resizable">
+                            <textarea class="" name="lettre_motivation" id="lettre_motivation" cols="16" rows="10" required="required" placeholder=""></textarea>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="secteur">Curriculum Vitae (CV) </label>
+                      <input type="file" id="cv" name="cv" required="required" class="form-control" placeholder="" style="width:34em">
+                    </div>
+                    <div class="row form-group">
+                      <div class="col-md-8 col-sm-12">
+                        <button type="submit" class="btn btn-block btn-primary btn-md" id="submit-btn"> Postuler</button>
+                      </div>
+                      <div class="col-md-4 col-sm-0 text-end">
+                        <!-- Utilisez cette colonne pour espacer le bouton sur les grands écrans -->
+                      </div>
+                    </div>
+                  </form>
+
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h4 class="modal-title" id="myModalLabel">JobBénin</h4>
+                        </div>
+                        <div class="modal-body">
+                          <p> <b> Succès!</b></p>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                  <script>
+                    $(document).ready(function() {
+                      $('#my-form').on('submit', function(e) {
+                        var isFormValid = true;
+                        var form = $(this);
+
+                        // Check if all required fields are filled
+                        form.find('[required]').each(function() {
+                          if (!$(this).val()) {
+                            isFormValid = false;
+                            $(this).addClass('is-invalid');
+                          }
+                        });
+
+
+                        if (!isFormValid) {
+                          e.preventDefault();
+                          return false;
+                        }
+
+
+                        form.unbind('submit').submit();
+
+                        $('#myModal').modal('show');
+
+
+                        e.preventDefault();
+                        return false;
+                      });
+                    });
+                  </script>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+
+
 </main>
 @endsection
